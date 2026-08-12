d2daec0f5064bb5b06760acb10ebabf25ffa673b
hankOS <hank@pigford.server>
2026-08-11 19:37:57 -0500

    fix: declare document language

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,5 +4,6 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_inertia_component "Home"
+    assert_select "html[lang='en']"
   end
 end
